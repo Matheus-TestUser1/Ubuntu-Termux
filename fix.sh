8571d52e60557e9ec9e7d659b30e5935ef4bb5cb
Matheus-TestUser1 <99508668+Matheus-TestUser1@users.noreply.github.com>
2022-05-26 18:32:35 -0300

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,2 +1,6 @@
-
-apt update -y && apt upgrade -y && apt install udisks2 -y && echo "" > /var/lib/dpkg/info/udisks2.postinst && dpkg --configure -a && apt-mark hold udisks2
+apt update -y
+apt upgrade -y
+apt install udisks2 -y
+echo "" > /var/lib/dpkg/info/udisks2.postinst
+dpkg --configure -a
+apt-mark hold udisks2
